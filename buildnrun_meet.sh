ceda6887ac77f6e79f9f63879b927a98c861f362
Gymnae <git@grundstil.de>
2026-09-22 22:06:04 +0200

    remve running as root

--- buildnrun_meet.sh
+++ buildnrun_meet.sh
@@ -4,6 +4,14 @@
 DATA_HOST_DIR="$(dirname "$(realpath "$0")")/data"
 mkdir -p "$DATA_HOST_DIR"
 
+# The container runs as unprivileged user 'meet' (uid/gid 10001).
+# Make the host data dir writable for it (needs root or ownership of the dir).
+if [ "$(id -u)" = "0" ]; then
+  chown -R 10001:10001 "$DATA_HOST_DIR"
+else
+  sudo chown -R 10001:10001 "$DATA_HOST_DIR" 2>/dev/null || chmod -R a+rwX "$DATA_HOST_DIR"
+fi
+
 docker build -t meet .
 docker kill meet
 docker rm meet
